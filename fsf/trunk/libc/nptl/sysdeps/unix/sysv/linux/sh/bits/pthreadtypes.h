5dddc2174894f6a4862dcf4b020d09a809017f1d
eglibc <eglibc@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2012-02-28 00:02:02 +0000

    Import glibc-mainline for 2012-02-28

--- fsf/trunk/libc/nptl/sysdeps/unix/sysv/linux/sh/bits/pthreadtypes.h
+++ fsf/trunk/libc/nptl/sysdeps/unix/sysv/linux/sh/bits/pthreadtypes.h
@@ -37,13 +37,13 @@
 typedef unsigned long int pthread_t;
 
 
-union __pthread_attr
+union pthread_attr_t
 {
   char __size[__SIZEOF_PTHREAD_ATTR_T];
   long int __align;
 };
 #ifndef __have_pthread_attr_t
-typedef union __pthread_attr pthread_attr_t;
+typedef union pthread_attr_t pthread_attr_t;
 # define __have_pthread_attr_t	1
 #endif
 

6c052be02056bc8a314a2f3a9840766890ec1834
eglibc <eglibc@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2013-02-15 00:02:01 +0000

    Import glibc-mainline for 2013-02-15

--- fsf/trunk/libc/sysdeps/powerpc/powerpc32/power4/fpu/mpa.c
+++ fsf/trunk/libc/sysdeps/powerpc/powerpc32/power4/fpu/mpa.c
@@ -105,8 +105,6 @@ __cpy (const mp_no *x, mp_no *y, int p)
   EY = EX;
   for (i = 0; i <= p; i++)
     Y[i] = X[i];
-
-  return;
 }
 
 /* Convert a multiple precision number *X into a double precision
@@ -181,7 +179,6 @@ norm (const mp_no *x, double *y, int p)
     c *= RADIXI;
 
   *y = c;
-  return;
 #undef R
 }
 
@@ -293,8 +290,6 @@ denorm (const mp_no *x, double *y, int p)
   c = X[0] * ((z[1] + R * (z[2] + R * z[3])) - TWO10);
 
   *y = c * TWOM1032;
-  return;
-
 #undef R
 }
 
@@ -359,7 +354,6 @@ __dbl_mp (double x, mp_no *y, int p)
     }
   for (; i <= p2; i++)
     Y[i] = ZERO;
-  return;
 }
 
 /* Add magnitudes of *X and *Y assuming that abs (*X) >= abs (*Y) > 0.  The
@@ -493,8 +487,6 @@ sub_magnitudes (const mp_no *x, const mp_no *y, mp_no *z, int p)
     Z[k++] = Z[i++];
   for (; k <= p2;)
     Z[k++] = ZERO;
-
-  return;
 }
 
 /* Add *X and *Y and store the result in *Z.  X and Y may overlap, but not X
@@ -544,7 +536,6 @@ __add (const mp_no *x, const mp_no *y, mp_no *z, int p)
       else
 	Z[0] = ZERO;
     }
-  return;
 }
 
 /* Subtract *Y from *X and return the result in *Z.  X and Y may overlap but
@@ -595,7 +586,6 @@ __sub (const mp_no *x, const mp_no *y, mp_no *z, int p)
       else
 	Z[0] = ZERO;
     }
-  return;
 }
 
 /* Multiply *X and *Y and store result in *Z.  X and Y may overlap but not X
@@ -684,7 +674,106 @@ __mul (const mp_no *x, const mp_no *y, mp_no *z, int p)
     EZ = EX + EY;
 
   Z[0] = X[0] * Y[0];
-  return;
+}
+
+/* Square *X and store result in *Y.  X and Y may not overlap.  For P in
+   [1, 2, 3], the exact result is truncated to P digits.  In case P > 3 the
+   error is bounded by 1.001 ULP.  This is a faster special case of
+   multiplication.  */
+void
+__sqr (const mp_no *x, mp_no *y, int p)
+{
+  long i, j, k, ip;
+  double u, yk;
+
+  /* Is z=0?  */
+  if (__glibc_unlikely (X[0] == ZERO))
+    {
+      Y[0] = ZERO;
+      return;
+    }
+
+  /* We need not iterate through all X's since it's pointless to
+     multiply zeroes.  */
+  for (ip = p; ip > 0; ip--)
+    if (X[ip] != ZERO)
+      break;
+
+  k = (__glibc_unlikely (p < 3)) ? p + p : p + 3;
+
+  while (k > 2 * ip + 1)
+    Y[k--] = ZERO;
+
+  yk = ZERO;
+
+  while (k > p)
+    {
+      double yk2 = 0.0;
+      long lim = k / 2;
+
+      if (k % 2 == 0)
+        {
+	  yk += X[lim] * X[lim];
+	  lim--;
+	}
+
+      /* In __mul, this loop (and the one within the next while loop) run
+         between a range to calculate the mantissa as follows:
+
+         Z[k] = X[k] * Y[n] + X[k+1] * Y[n-1] ... + X[n-1] * Y[k+1]
+		+ X[n] * Y[k]
+
+         For X == Y, we can get away with summing halfway and doubling the
+	 result.  For cases where the range size is even, the mid-point needs
+	 to be added separately (above).  */
+      for (i = k - p, j = p; i <= lim; i++, j--)
+	yk2 += X[i] * X[j];
+
+      yk += 2.0 * yk2;
+
+      u = (yk + CUTTER) - CUTTER;
+      if (u > yk)
+	u -= RADIX;
+      Y[k--] = yk - u;
+      yk = u * RADIXI;
+    }
+
+  while (k > 1)
+    {
+      double yk2 = 0.0;
+      long lim = k / 2;
+
+      if (k % 2 == 0)
+        {
+	  yk += X[lim] * X[lim];
+	  lim--;
+	}
+
+      /* Likewise for this loop.  */
+      for (i = 1, j = k - 1; i <= lim; i++, j--)
+	yk2 += X[i] * X[j];
+
+      yk += 2.0 * yk2;
+
+      u = (yk + CUTTER) - CUTTER;
+      if (u > yk)
+	u -= RADIX;
+      Y[k--] = yk - u;
+      yk = u * RADIXI;
+    }
+  Y[k] = yk;
+
+  /* Squares are always positive.  */
+  Y[0] = 1.0;
+
+  EY = 2 * EX;
+  /* Is there a carry beyond the most significant digit?  */
+  if (__glibc_unlikely (Y[1] == ZERO))
+    {
+      for (i = 1; i <= p; i++)
+	Y[i] = Y[i + 1];
+      EY--;
+    }
 }
 
 /* Invert *X and store in *Y.  Relative error bound:
@@ -718,7 +807,6 @@ __inv (const mp_no *x, mp_no *y, int p)
       __sub (&mptwo, y, &z, p);
       __mul (&w, &z, y, p);
     }
-  return;
 }
 
 /* Divide *X by *Y and store result in *Z.  X and Y may overlap but not X and Z
@@ -740,5 +828,4 @@ __dvd (const mp_no *x, const mp_no *y, mp_no *z, int p)
       __inv (y, &w, p);
       __mul (x, &w, z, p);
     }
-  return;
 }

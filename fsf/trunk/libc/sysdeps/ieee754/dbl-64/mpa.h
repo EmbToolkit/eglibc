cc6a324f05b96b5ba549dc41631c78264b3606aa
eglibc <eglibc@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2013-03-30 00:01:57 +0000

    Import glibc-mainline for 2013-03-30

--- fsf/trunk/libc/sysdeps/ieee754/dbl-64/mpa.h
+++ fsf/trunk/libc/sysdeps/ieee754/dbl-64/mpa.h
@@ -91,10 +91,6 @@ extern const mp_no mptwo;
 # define  TWO52     0x1.0p52		/* 2^52    */
 #endif
 
-#define  ZERO      0.0			/* 0       */
-#define  MZERO     -0.0			/* 0 with the sign bit set */
-#define  ONE       1.0			/* 1       */
-#define  MONE      -1.0			/* -1      */
 #define  TWO       2.0			/*  2      */
 
 #define  TWO5      TWOPOW (5)		/* 2^5     */
@@ -152,10 +148,10 @@ __pow_mp (int pow, mp_no *y, int p)
       rem += 24;
     }
   /* The sign of any 2^x is always positive.  */
-  Y[0] = ONE;
+  Y[0] = 1;
   Y[1] = 1 << rem;
 
-  /* Everything else is ZERO.  */
+  /* Everything else is 0.  */
   for (i = 2; i <= p; i++)
-    Y[i] = ZERO;
+    Y[i] = 0;
 }

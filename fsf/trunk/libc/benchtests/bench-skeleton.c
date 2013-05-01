b3a75a16eed309769eb86a9d7a53ec8d2d1f75d9
eglibc <eglibc@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2013-05-01 00:02:09 +0000

    Import glibc-mainline for 2013-05-01

--- fsf/trunk/libc/benchtests/bench-skeleton.c
+++ fsf/trunk/libc/benchtests/bench-skeleton.c
@@ -22,13 +22,17 @@
 #include <time.h>
 #include <inttypes.h>
 
+#define TIMESPEC_AFTER(a, b) \
+  (((a).tv_sec == (b).tv_sec) ?						      \
+     ((a).tv_nsec > (b).tv_nsec) :					      \
+	((a).tv_sec > (b).tv_sec))
 int
 main (int argc, char **argv)
 {
-  unsigned long i, j, k;
-  uint64_t total = 0, max = 0, min = 0x7fffffffffffffff;
-  struct timespec start, end;
+  unsigned long i, k;
+  struct timespec start, end, runtime;
 
+  memset (&runtime, 0, sizeof (runtime));
   memset (&start, 0, sizeof (start));
   memset (&end, 0, sizeof (end));
 
@@ -39,37 +43,58 @@ main (int argc, char **argv)
      Measurements close to the minimum clock resolution won't make much sense,
      but it's better than having nothing at all.  */
   unsigned long iters = 1000 * start.tv_nsec;
-  unsigned long total_iters = ITER / iters;
 
-  for (i = 0; i < NUM_SAMPLES; i++)
+  for (int v = 0; v < NUM_VARIANTS; v++)
     {
-      for (j = 0; j < total_iters; j ++)
+      /* Run for approximately DURATION seconds.  */
+      clock_gettime (CLOCK_MONOTONIC_RAW, &runtime);
+      runtime.tv_sec += DURATION;
+
+      double d_total_i = 0;
+      uint64_t total = 0, max = 0, min = 0x7fffffffffffffff;
+      while (1)
 	{
-	  clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &start);
-	  for (k = 0; k < iters; k++)
-	    BENCH_FUNC(i);
-	  clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &end);
+	  for (i = 0; i < NUM_SAMPLES (v); i++)
+	    {
+	      clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &start);
+	      for (k = 0; k < iters; k++)
+		BENCH_FUNC (v, i);
+	      clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &end);
 
-	  uint64_t cur = (end.tv_nsec - start.tv_nsec
-			 + ((end.tv_sec - start.tv_sec)
-			    * (uint64_t) 1000000000));
+	      uint64_t cur = (end.tv_nsec - start.tv_nsec
+			      + ((end.tv_sec - start.tv_sec)
+				 * (uint64_t) 1000000000));
 
-	  if (cur > max)
-	    max = cur;
+	      if (cur > max)
+		max = cur;
 
-	  if (cur < min)
-	    min = cur;
+	      if (cur < min)
+		min = cur;
 
-	  total += cur;
+	      total += cur;
+
+	      d_total_i += iters;
+	    }
+	  struct timespec curtime;
+
+	  memset (&curtime, 0, sizeof (curtime));
+	  clock_gettime (CLOCK_MONOTONIC_RAW, &curtime);
+	  if (TIMESPEC_AFTER (curtime, runtime))
+	    goto done;
 	}
-    }
 
-  double d_total_s = total * 1e-9;
-  double d_iters = iters;
-  double d_total_i = (double)ITER * NUM_SAMPLES;
-  printf (FUNCNAME ": ITERS:%g: TOTAL:%gs, MAX:%gns, MIN:%gns, %g iter/s\n",
-	  d_total_i, d_total_s, max / d_iters, min / d_iters,
-	  d_total_i / d_total_s);
+      double d_total_s;
+      double d_iters;
+
+    done:
+      d_total_s = total * 1e-9;
+      d_iters = iters;
+
+      printf ("%s: ITERS:%g: TOTAL:%gs, MAX:%gns, MIN:%gns, %g iter/s\n",
+	      VARIANT (v),
+	      d_total_i, d_total_s, max / d_iters, min / d_iters,
+	      d_total_i / d_total_s);
+    }
 
   return 0;
 }

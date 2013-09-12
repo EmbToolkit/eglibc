ef106631b6a639afa08820043a8831f0d3d4e0e0
eglibc <eglibc@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2013-09-12 00:02:11 +0000

    Import glibc-mainline for 2013-09-12

--- fsf/trunk/libc/malloc/malloc.c
+++ fsf/trunk/libc/malloc/malloc.c
@@ -3015,6 +3015,13 @@ __libc_memalign(size_t alignment, size_t bytes)
   /* Otherwise, ensure that it is at least a minimum chunk size */
   if (alignment <  MINSIZE) alignment = MINSIZE;
 
+  /* Check for overflow.  */
+  if (bytes > SIZE_MAX - alignment - MINSIZE)
+    {
+      __set_errno (ENOMEM);
+      return 0;
+    }
+
   arena_get(ar_ptr, bytes + alignment + MINSIZE);
   if(!ar_ptr)
     return 0;
@@ -3045,6 +3052,13 @@ __libc_valloc(size_t bytes)
     ptmalloc_init ();
 
   size_t pagesz = GLRO(dl_pagesize);
+
+  /* Check for overflow.  */
+  if (bytes > SIZE_MAX - pagesz - MINSIZE)
+    {
+      __set_errno (ENOMEM);
+      return 0;
+    }
 
   void *(*hook) (size_t, size_t, const void *) =
     force_reg (__memalign_hook);
@@ -3081,6 +3095,13 @@ __libc_pvalloc(size_t bytes)
   size_t pagesz = GLRO(dl_pagesize);
   size_t page_mask = GLRO(dl_pagesize) - 1;
   size_t rounded_bytes = (bytes + page_mask) & ~(page_mask);
+
+  /* Check for overflow.  */
+  if (bytes > SIZE_MAX - 2*pagesz - MINSIZE)
+    {
+      __set_errno (ENOMEM);
+      return 0;
+    }
 
   void *(*hook) (size_t, size_t, const void *) =
     force_reg (__memalign_hook);
